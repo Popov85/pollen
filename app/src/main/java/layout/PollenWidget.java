7158c80630dd464999a29da34a2877ad625605da
Popov85 <andrey.popov85@gmail.com>
2017-05-15 15:36:00 +0300

    initial commit

---
+++ app/src/main/java/layout/PollenWidget.java
@@ -0,0 +1,51 @@
+package layout;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+import ua.edu.zsmy.biology.zsmu.pollen.R;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class PollenWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.pollen_widget);
+        views.setTextViewText(R.id.appwidget_text, "Запоріжжя, Україна");
+
+        /*views.setTextViewText(R.id.textView, "Today");
+        views.setTextViewText(R.id.textView3, "Tomorrow");
+        views.setTextViewText(R.id.textView4, "Sun 01.08");*/
+
+
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
